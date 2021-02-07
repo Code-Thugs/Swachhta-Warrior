dd40374e4c5c5b1425a73bb1141dd931a1dc9934
Sameer Chauhan <sameer55chauhan@gmail.com>
2021-02-08 00:08:43 +0530

    Navigation Structure

---
+++ Home.js
@@ -0,0 +1,11 @@
+import { View, Text} from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Welcome to Home!</Text>
+        </View>
+    );
+};
+
+export default Home;
